ce607b30319b3238a364f86cf86de81d9aba166e
Marcin Federowicz <marcin.federowicz@gmail.com>
2025-03-02 00:20:51 +0100

    comments: create show comment

--- handlers/comments_comments_show_handler.go
+++ handlers/comments_comments_show_handler.go
@@ -3,6 +3,8 @@ package handlers
 
 import (
 	"errors"
+	"fmt"
+	"net/http"
 
 	"github.com/mfederowicz/trakt-sync/consts"
 	"github.com/mfederowicz/trakt-sync/internal"
@@ -15,11 +17,35 @@ type CommentsCommentsShowHandler struct{ common CommonLogic }
 
 // Handle to handle comments: show type
 func (h CommentsCommentsShowHandler) Handle(options *str.Options, client *internal.Client) error {
-	printer.Println("generate comment:", options.Type)
-
 	if options.TraktID == consts.ZeroValue {
 		return errors.New(consts.EmptyTraktIDMsg)
-	}	
+	}
+
+	connections, _ := h.common.FetchUserConnections(client, options)
+	show, err := h.common.FetchShow(client, options)
+	if err != nil {
+		return fmt.Errorf("fetch show error:%w", err)
+	}
+
+	c := new(str.Comment)
+	c.Show = show
+	if len(options.Comment) > consts.ZeroValue {
+		c.Comment = &options.Comment
+	}
+	c.Spoiler = &options.Spoiler
+	c.Sharing = new(str.Sharing)
+	c.Sharing.Tumblr = connections.Tumblr
+	c.Sharing.Twitter = connections.Twitter
+	c.Sharing.Mastodon = connections.Mastodon
+
+	result, resp, err := h.common.Comment(client, c)
+	if err != nil {
+		return fmt.Errorf("comment error:%w", err)
+	}
+	
+	if resp.StatusCode == http.StatusCreated {
+		printer.Printf("result: success, show comment number:%d \n", result.ID)
+	}
 
 	return nil
 }
