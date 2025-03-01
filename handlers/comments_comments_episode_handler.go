582a6b0512c8c86b97d56788dfe41b101ea1ff26
Marcin Federowicz <marcin.federowicz@gmail.com>
2025-03-02 00:58:58 +0100

    comments: create episode comment

--- handlers/comments_comments_episode_handler.go
+++ handlers/comments_comments_episode_handler.go
@@ -3,6 +3,8 @@ package handlers
 
 import (
 	"errors"
+	"fmt"
+	"net/http"
 
 	"github.com/mfederowicz/trakt-sync/consts"
 	"github.com/mfederowicz/trakt-sync/internal"
@@ -15,11 +17,33 @@ type CommentsCommentsEpisodeHandler struct{ common CommonLogic }
 
 // Handle to handle comments: episode type
 func (h CommentsCommentsEpisodeHandler) Handle(options *str.Options, client *internal.Client) error {
-	printer.Println("generate comment:", options.Type)
-
 	if options.TraktID == consts.ZeroValue {
 		return errors.New(consts.EmptyTraktIDMsg)
-	}	
+	}
+	connections, _ := h.common.FetchUserConnections(client, options)
+	episode, err := h.common.FetchEpisode(client, options)
+	if err != nil {
+		return fmt.Errorf("fetch episode error:%w", err)
+	}
 
+	c := new(str.Comment)
+	c.Episode = episode
+	if len(options.Comment) > consts.ZeroValue {
+		c.Comment = &options.Comment
+	}
+	c.Spoiler = &options.Spoiler
+	c.Sharing = new(str.Sharing)
+	c.Sharing.Tumblr = connections.Tumblr
+	c.Sharing.Twitter = connections.Twitter
+	c.Sharing.Mastodon = connections.Mastodon
+
+	result, resp, err := h.common.Comment(client, c)
+	if err != nil {
+		return fmt.Errorf("comment error:%w", err)
+	}
+
+	if resp.StatusCode == http.StatusCreated {
+		printer.Printf("result: success, episode comment number:%d \n", result.ID)
+	}
 	return nil
 }
