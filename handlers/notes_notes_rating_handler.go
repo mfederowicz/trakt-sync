e5e03db2f59888be6fa2a42a5c99b29eca9f8a69
Marcin Federowicz <marcin.federowicz@gmail.com>
2025-03-31 23:30:25 +0200

    notes:rating notes

--- handlers/notes_notes_rating_handler.go
+++ handlers/notes_notes_rating_handler.go
@@ -2,7 +2,13 @@
 package handlers
 
 import (
+	"errors"
+	"fmt"
+	"net/http"
+
+	"github.com/mfederowicz/trakt-sync/consts"
 	"github.com/mfederowicz/trakt-sync/internal"
+	"github.com/mfederowicz/trakt-sync/printer"
 	"github.com/mfederowicz/trakt-sync/str"
 )
 
@@ -11,5 +17,41 @@ type NotesNotesRatingHandler struct{ common CommonLogic }
 
 // Handle to handle comments: movie type
 func (h NotesNotesRatingHandler) Handle(options *str.Options, client *internal.Client) error {
+	if len(options.InternalID) == consts.ZeroValue {
+		return errors.New(consts.EmptyMovieIDMsg)
+	}
+	n := new(str.Notes)
+	n.Notes = &options.Notes
+	a := new(str.AttachedTo)
+	t := "rating"
+	a.Type = &t
+	n.AttachedTo = a
+	switch options.Item {
+	case "movie":
+		movie, _, _ := h.common.FetchMovie(client, options)
+		n.Movie = movie
+	case "show":
+		show, _ := h.common.FetchShow(client, options)
+		n.Show = show
+	case "season":
+		season, _ := h.common.FetchSeason(client, options)
+		n.Season = season
+	case "episode":
+		episode, _ := h.common.FetchEpisode(client, options)
+		n.Episode = episode
+	default:
+		h.common.GenActionTypeItemUsage(options, []string{"movie", "show", "season", "episode"})
+		return nil
+	}
+	p := "private"
+	n.Privacy = &p
+	result, resp, err := h.common.Notes(client, n)
+	if err != nil {
+		return fmt.Errorf("%w", err)
+	}
+
+	if resp.StatusCode == http.StatusCreated {
+		printer.Printf("result: success, rating notes number:%d \n", result.ID)
+	}
 	return nil
 }
