5fae72bfa8eab679b4607855622908a2c0eee687
Marcin Federowicz <marcin.federowicz@gmail.com>
2026-06-16 20:16:08 +0200

    wtached: add pagination

--- handlers/users_watched_handler.go
+++ handlers/users_watched_handler.go
@@ -5,7 +5,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"time"
 
+	"github.com/mfederowicz/trakt-sync/consts"
 	"github.com/mfederowicz/trakt-sync/internal"
 	"github.com/mfederowicz/trakt-sync/printer"
 	"github.com/mfederowicz/trakt-sync/str"
@@ -19,7 +21,7 @@ type UsersWatchedHandler struct{}
 // Handle to handle users: watched action
 func (UsersWatchedHandler) Handle(options *str.Options, client *internal.Client) error {
 	printer.Println("fetch watched for:" + options.UserName + " and type:" + options.Type)
-	watched, resp, err := fetchUsersWatched(client, options)
+	watched, resp, err := fetchUsersWatched(client, options, consts.DefaultPage)
 	if err != nil {
 		return fmt.Errorf("fetch user watched error:%w", err)
 	}
@@ -37,16 +39,34 @@ func (UsersWatchedHandler) Handle(options *str.Options, client *internal.Client)
 	return nil
 }
 
-func fetchUsersWatched(client *internal.Client, options *str.Options) ([]*str.UserWatched, *str.Response, error) {
+func fetchUsersWatched(client *internal.Client, options *str.Options, page int) ([]*str.UserWatched, *str.Response, error) {
 	username := options.UserName
 	watchType := options.Type
-	opts := uri.ListOptions{Extended: options.ExtendedInfo}
+	opts := uri.ListOptions{Page: page, Limit: consts.PerPage, Extended: options.ExtendedInfo}
 	watched, resp, err := client.Users.GetWatched(
 		client.BuildCtxFromOptions(options),
 		&username,
 		&watchType,
 		&opts,
 	)
+	if err != nil {
+		return nil, resp, fmt.Errorf("fetch user watched error:%w", err)
+	}
 
-	return watched, resp, err
+	// Check if there are more pages
+	if client.HavePages(page, resp, options.PagesLimit) {
+		time.Sleep(time.Duration(consts.SleepNumberOfSeconds) * time.Second)
+
+		// Fetch items from the next page
+		nextPage := page + consts.NextPageStep
+		nextPageItems, resp, err := fetchUsersWatched(client, options, nextPage)
+		if err != nil {
+			return nil, resp, err
+		}
+
+		// Append items from the next page to the current page
+		watched = append(watched, nextPageItems...)
+	}
+
+	return watched, resp, nil
 }
